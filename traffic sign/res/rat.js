f71b36355df2b930fd7d4c36608de642466be8f7
CobyMorrone <124190972+CobyMorrone@users.noreply.github.com>
2023-04-03 10:28:21 -0500

    Traffic sign with updated interstate sign :)

---
+++ traffic sign/res/rat.js
@@ -0,0 +1,15 @@
+var rats =
+[
+{
+  'Better' :      ['Worser','Greater','Smaller'],
+  'be' :          ['be','not to be','having fun'],
+  'a Samurai' :   ['a Samurai','no one','a Gardener','yourself'],
+  'in a Garden' : ['in a Garden','in the Heaven','nowhere','at War'],
+  'than' :        ['than']
+},
+{
+  'You' :      ['I','Grandma','Curiosity'],
+  'killed' :          ['received','painted','made to purr'],
+  'a rat' :   ['no one','a Gardener','a Samurai','the cat','drivers','yourself'],
+}
+];
